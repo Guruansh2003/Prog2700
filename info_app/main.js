54f268905db0c05f4f9402a04199047fe40d341f
Guruansh2003 <144456530+Guruansh2003@users.noreply.github.com>
2024-02-06 14:07:34 +0000

    image_app

---
+++ info_app/main.js
@@ -0,0 +1,38 @@
+function chargebattery() {
+    var a= document.getElementById("div1");
+    a.innerHTML = "&#xf244;";
+    setTimeout(function () {
+        a.innerHTML = "&#xf243;";
+      }, 1000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf242;";
+      }, 2000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf241;";
+      }, 3000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf240;";
+      }, 4000);
+  }
+  chargebattery();
+  setInterval(chargebattery, 5000);
+
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
